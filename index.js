9fb1dbecc528f11f6dac6bd26c6938502286ae7a
Md Sholayman <mdsholayman998@gmail.com>
2023-03-01 18:55:35 +0600

    single data loaded

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
@@ -15,10 +15,32 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-console.log(uri);
+
 
 async function run() {
   try {
+  const shoesCollection = client.db("shoe-store").collection("shoes")
+  const sliderCollection = client.db("shoe-store").collection("sliderImage")
+
+  app.get("/products" , async(req,res) =>{
+    const cursor = shoesCollection.find({})
+    const result = await cursor.toArray()
+    res.send(result) 
+  }) ;
+
+  app.get("/product/:id" , async(req,res) =>{
+    const id = req.params.id ;
+    const query = { _id : new ObjectId(id)} ;
+    const result = await shoesCollection.findOne(query) ;
+    res.send(result) ;
+   })
+
+  app.get("/slider" , async(req,res) =>{
+   const cursor = sliderCollection.find({})
+   const result = await cursor.toArray()
+   res.send(result)
+  })
+
   } finally {
   }
 }
